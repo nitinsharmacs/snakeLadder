9a5c4a1559adb741df929c02203b2b0c7d08a575
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-04-23 14:35:03 +0530

    Abstracted game object
      Working for multiple players
      Snakes and ladders as objects in board

--- snakeLadder.js
+++ snakeLadder.js
@@ -2,30 +2,77 @@ const throwDice = function () {
   return Math.ceil(Math.random() * 6);
 };
 
-const changeTile = function (player, steps) {
-  const newTile = player.tile + steps;
-  return { name: player.name, tile: newTile };
+const movePlayer = function (player, tile) {
+  return { name: player.name, tile: tile };
 };
 
-const isGameEnded = function (player, board) {
+const isGameOver = function (player, board) {
   return player.tile >= board.boardSize;
 };
 
-const startGame = function () {
-  const board = {
-    snakes: [13, 15],
-    ladders: [9, 3],
-    boardSize: 16
-  };
-  let player = {
-    name: 'john',
-    tile: 0
-  };
-  while (!isGameEnded(player, board)) {
-    const diceValue = throwDice();
-    player = changeTile(player, diceValue);
-    console.log(player);
-  }
-}
+const snakeOn = function (tile, snakes) {
+  return snakes[tile];
+};
 
-startGame();
+const ladderOn = function (tile, ladders) {
+  return ladders[tile];
+};
+
+const nextTile = function (currentTile, board) {
+  const snakeTail = snakeOn(currentTile, board.snakes);
+  const ladderTop = ladderOn(currentTile, board.ladders);
+  return snakeTail || ladderTop || currentTile;
+};
+
+const playMove = function (player, board) {
+  const diceValue = throwDice();
+  const newTile = player.tile + diceValue;
+  return movePlayer(player, nextTile(newTile, board));
+};
+
+const startGame = function (game) {
+  const board = game.board;
+  const players = game.players;
+  let moves = 0;
+
+  while (true) {
+    const currentPlayerIndex = moves % players.length;
+    let currentPlayer = players[currentPlayerIndex];
+
+    currentPlayer = playMove(currentPlayer, board);
+    players[currentPlayerIndex] = currentPlayer;
+
+    if (isGameOver(currentPlayer, board)) {
+      return currentPlayer;
+    }
+    moves += 1;
+  }
+};
+
+const game = {
+  board: {
+    snakes: {
+      '14': 4,
+      '10': 2
+    },
+    ladders: {
+      '3': 9,
+      '8': 15
+    },
+    boardSize: 16
+  },
+  players: [
+    {
+      name: 'john',
+      tile: 0
+    },
+    {
+      name: 'hemant',
+      tile: 0
+    }
+  ]
+}
+console.log(startGame(game).name, 'won!');
+console.log(game);
+
+// todo : change the name of the function playMove()
